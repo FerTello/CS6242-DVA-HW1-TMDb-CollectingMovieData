d0224ec826321327ca572480113a0e21d2c4722b
Fernanda Tello <Fernanda@Fernandas-MacBook-Pro.local>
2018-09-08 14:05:46 -0500

    last version of APIcall using http.client

--- APIcall.py
+++ APIcall.py
@@ -1,6 +1,7 @@
 import http.client
 import getopt
 import sys
+import time
 
 conn = http.client.HTTPSConnection("api.themoviedb.org")
 
@@ -11,14 +12,11 @@ def get_api_key():
     argv = getopt.getopt(sys.argv[1:], "")
     return argv[1]
 
-print(get_api_key())
 
 api_key = get_api_key()
 my_api_key = api_key[0]
-print(api_key[0])
 
-conn.request("GET", "/3/discover/movie?with_runtime.lte=0&with_genres=35&primary_release_date.gte=2000&page=2&sort_by=popularity.desc&api_key=" + my_api_key, payload)
-
+conn.request("GET", "/3/discover/movie?with_runtime.lte=0&with_genres=35&primary_release_date.gte=2000&page=1&sort_by=popularity.desc&language=en-US&api_key="+my_api_key, payload)
 res = conn.getresponse()
 data = res.read()
 a = conn.timeout
