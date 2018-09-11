d4e4206c9eb1a2fade6a0d766961d09626e554f2
Fernanda Tello <Fernanda@Fernandas-MacBook-Pro.local>
2018-09-10 19:19:35 -0500

    added code to remove duplicated pairs

--- duplicates.py
+++ duplicates.py
@@ -1,5 +1,4 @@
 
-
 listMovie = ['12345', '12345', '12345', '12345', '33333', '33333', '98765', '98765']
 listSimMovie = ['98765', '55555', '33333', '44444', '55555', '12345', '12345', '77777']
 
@@ -11,10 +10,9 @@ for i in range(0, len(listMovie)):
 print(listBoth)
 count = 0
 
-delDuplicates = ['']*len(listBoth)
-a = 0
+delDuplicates = []
 
-# filling in duplicates to be removed in delduplicates[]
+# filling in duplicates to be removed in delDuplicates[]
 for i in range(0, len(listBoth)):
     j = i + 1
     for j in range(i + 1, len(listBoth)):
@@ -23,13 +21,11 @@ for i in range(0, len(listBoth)):
             if listBoth[j][0] == listBoth[i][1]:
                 print("duplicate found")
                 if listBoth[i][0] < listBoth[j][0]:
-                    delDuplicates[a] = listBoth[j]
-                    a = a + 1
+                    delDuplicates.append(listBoth[j])
                 else:
-                    delDuplicates[a] = listBoth[i]
-                    a = a + 1
+                    delDuplicates.append(listBoth[i])
 
-# remove duplicates from listBoth
+# remove duplicates from listBoth[]
 for i in range(len(delDuplicates)):
     for j in range(len(listBoth)):
         if delDuplicates[i] == listBoth[j]:
@@ -40,3 +36,14 @@ for i in range(len(delDuplicates)):
 print("listBoth: " + str(listBoth))
 print("delDuplicates: " + str(delDuplicates))
 
+
+a = 4
+b = 5
+dumList = []
+dumList.append(['1', '1'])
+dumList.append([str(a), str(b)])
+dumList.append(['3', '3'])
+dumList.append(['4', '4'])
+
+
+print("dumList: " + str(dumList))
--- script.py
+++ script.py
@@ -23,9 +23,9 @@ for p in range(1, 16):
     s1 = json.dumps(j)
     json_parsed = json.loads(s1)
     keys = j.keys()
-    time.sleep(5)
+    time.sleep(1)
 
-#Q1.1b
+# Q1.1b
     for key in j.keys():
         if key == 'results':
             results = j[key]
@@ -39,51 +39,71 @@ for p in range(1, 16):
                         print(str(movie_id)+","+movie_title, file=open("movie_ID_name.csv", "a+"))
 
 
-# #Q1.1c
-# count = 0
-# file = open("movie_ID_sim_movie_ID.csv", "w+")
-#
-# with open('movie_ID_name.csv') as csvFile:
-#     readCSV = csv.reader(csvFile, delimiter=',')
-#     popMoviesIDs = []
-#     for row in readCSV:
-#         ID = row[0]
-#
-#         popMoviesIDs.append(ID)
-#
-#     print(popMoviesIDs)
-#     print(popMoviesIDs[0])
-#
-#
-# for index in range(0, 300):
-#     print(index)
-#     res2 = requests.get("https://api.themoviedb.org/3/movie/"+popMoviesIDs[index]+"/similar?api_key=" + api_key + "&language=en-US&page=1")
-#     sim = res2.json()
-#     sim_str = json.dumps(sim)
-#     sim_parsed = json.loads(sim_str)
-#     # print("popMoviesIDs[index]: "+popMoviesIDs[index])
-#     # print("sim_parsed: ")
-#     # print(sim_parsed)
-#     # print(sim.keys())
-#     for key2 in sim.keys():
-#         if key2 == 'results':
-#             sim_results = sim[key2]
-#             # print(len(sim_results))
-#             if len(sim_results) > 0:
-#                 for a in range(0, len(sim_results)): #only need 5 similar movies (ids)
-#                     if len(sim_results) <= 5:
-#                         print("+++++++++++++++++++++++"+str(len(sim_results))+" similar movies of" + popMoviesIDs[index])
-#                     if a == 5:
-#                         print("-------------"+str(a)+" all similar movies of " + popMoviesIDs[index])
-#                         break
-#                     for key3 in sim_results[a].keys(): #14 items
-#                         if key3 == 'id':
-#                             sim_movie_id = sim_results[a][key3]
-#                             sim_movie_title = sim_results[a]['title']
-#                             # print(popMoviesIDs[index]+","+str(sim_movie_id))
-#                             print(popMoviesIDs[index]+","+str(sim_movie_id), file=open("movie_ID_sim_movie_ID.csv", "a+"))
-#                     count = count + 1
-#             else:
-#                 print(popMoviesIDs[index]+" has no similar movies")
-#
-# print("count= "+str(count))
+# Q1.1c
+file = open("movie_ID_sim_movie_ID.csv", "w+")
+
+with open('movie_ID_name.csv') as csvFile:
+    readCSV = csv.reader(csvFile, delimiter=',')
+    popMoviesIDs = []
+    for row in readCSV:
+        ID = row[0]
+
+        popMoviesIDs.append(ID)
+
+    print(popMoviesIDs)
+    print(popMoviesIDs[0])
+
+listBoth = []
+for index in range(0, 300):
+    print(index)
+    res2 = requests.get("https://api.themoviedb.org/3/movie/"+popMoviesIDs[index]+"/similar?api_key=" + api_key + "&language=en-US&page=1")
+    sim = res2.json()
+    sim_str = json.dumps(sim)
+    sim_parsed = json.loads(sim_str)
+    for key2 in sim.keys():
+        if key2 == 'results':
+            sim_results = sim[key2]
+            if len(sim_results) > 0:
+                for a in range(0, len(sim_results)):  # only need 5 similar movies (ids)
+                    if len(sim_results) <= 5:
+                        print("+++++++++++++++++++++++"+str(len(sim_results))+" similar movies of" + popMoviesIDs[index])
+                    if a == 5:
+                        print("-------------"+str(a)+" all similar movies of " + popMoviesIDs[index])
+                        break
+                    for key3 in sim_results[a].keys():  # 14 items
+                        if key3 == 'id':
+                            sim_movie_id = sim_results[a][key3]
+                            print(popMoviesIDs[index]+","+str(sim_movie_id), file=open("movie_ID_sim_movie_ID.csv", "a+"))
+                            listBoth.append([str(popMoviesIDs[index]), str(sim_movie_id)])
+                else:
+                    print(popMoviesIDs[index]+" has no similar movies")
+
+
+delDuplicates = []
+a = 0
+
+# filling in duplicates to be removed in delDuplicates[]
+for i in range(0, len(listBoth)):
+    j = i + 1
+    for j in range(i + 1, len(listBoth)):
+        if listBoth[i][0] == listBoth[j][1]:
+            print("possible duplicate")
+            if listBoth[j][0] == listBoth[i][1]:
+                print("duplicate found")
+                if listBoth[i][0] < listBoth[j][0]:
+                    delDuplicates.append(listBoth[j])
+                else:
+                    delDuplicates.append(listBoth[i])
+
+print("len(listBoth) before dup rem: " + str(len(listBoth)))
+for i in range(len(delDuplicates)):
+    for j in range(len(listBoth)):
+        if delDuplicates[i] == listBoth[j]:
+            del listBoth[j]
+            break
+
+print("len(listBoth) after dup rem: " + str(len(listBoth)))
+print("listBoth: " + str(listBoth))
+print("len(delDuplicates): " + str(len(delDuplicates)))
+print("delDuplicates: " + str(delDuplicates))
+
